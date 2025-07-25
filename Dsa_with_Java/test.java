c8a238f0d6b047d19a5bc65d7c2464fee8fcc106
bablu kumar <bablu707041dk@gmail.com>
2025-07-25 14:49:28 +0530

    hello

--- Dsa_with_Java/test.java
+++ Dsa_with_Java/test.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class test {
+    public static void main(String[] args){
+        int[][] arr = {{1,2},{3,4}};
+        for(int[] n : arr){
+            System.out.println(n);
+        }
+    }
 }
